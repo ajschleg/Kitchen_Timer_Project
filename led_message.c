a770836c6a26f075dd1fb4a6cfa81b68564baf5f
Austin Schlegel <44715741+ajschleg@users.noreply.github.com>
2019-03-30 19:35:33 -0400

    Able to blink 1 red LED. Circuit shown in Led1.jpg

--- led_message.c
+++ led_message.c
@@ -1,4 +1,6 @@
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <xc.h>
 #include "led_message.h"
 
@@ -10,7 +12,19 @@
  */
 
 
-void main(void) {
-    while(1){}
-    return;
+void main(void) 
+{  
+    TRISBbits.RB0 = 0;
+    OSCCON = 0x76;
+    //LATB = 0xFF;
+    //LATBbits.LATB0 = 1;
+    //PORTB= 0x01;
+    while(1)
+    {
+        LATBbits.LATB0 =~LATBbits.LATB0;
+        for(int i = 0; i < 20; i++)
+        {
+            __delay_ms(50);
+        }
+    }
 }
