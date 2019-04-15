caabdebd8cd987e9ce7123cf57a40a5ad15f9aaa
Austin Schlegel <44715741+ajschleg@users.noreply.github.com>
2019-04-14 20:41:05 -0400

    Init speaker files

---
+++ Speaker.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Speaker.h
+ * Author: austinschlegel
+ *
+ * Created on April 14, 2019, 8:40 PM
+ */
+
+#ifndef SPEAKER_H
+#define	SPEAKER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SPEAKER_H */
+
